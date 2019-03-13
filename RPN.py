3e35cae0af95496cf14c0fc9060da7ed029fecdd
Tim Fielder <fiel1139@gmail.com>
2019-03-13 15:47:27 -0600

    Adds additional conditionalstatements to run calculator.

--- RPN.py
+++ RPN.py
@@ -2,6 +2,7 @@
 def main():
     welcome_message()
     print_instructions()
+    run_calculator()
     farwell_message()
 
 def welcome_message():
@@ -18,6 +19,24 @@ def print_instructions():
     print("in the stack to conduct an operation.")
     print("To empty the stack or calculator, type 'c'.")
     print("To print the current stack, type 'p'.")
+
+def run_calculator():
+    response = ''
+    while response != 'q':
+        response = input()
+        if response == 'i':
+            print_instructions()
+        if response == 'c':
+            print("you are clearing a stack")
+        if response == 'p':
+            print("you are printing the stack")
+        ##if valid_input?(response):
+            ##add_to_stack(response)
+        ##if input_operator?(response):
+            ##attempt_calculation(response)
+        ##if !valid_input?(response):
+            ##print('Try typing a number or an operator.')
+
 
 def farwell_message():
     print('goodbye')
